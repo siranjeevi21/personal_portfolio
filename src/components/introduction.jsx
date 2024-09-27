c051ae8643d9759e2e869e2483227f6c28a4df11
Siranjeevi <66528036+siranjeevi21@users.noreply.github.com>
2024-09-27 13:24:17 +0530

    Update introduction.jsx
    Updated Resume link

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1 style={{color: 'white'}}>I'm <br />Siranjeevi <br />Subramaniyan</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1CKvj-KIhOPUujKwH9ZkFqCYi_l7MRDuC/view?usp=sharing" target="_blank" rel="noopener noreferrer" style={{color: 'white', border: '2px solid white'}}>View Resume <i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1AGTLJ9UD5eX7IwbTqNTcziAR0JDPoCBZ/view?usp=sharing" target="_blank" rel="noopener noreferrer" style={{color: 'white', border: '2px solid white'}}>View Resume <i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
